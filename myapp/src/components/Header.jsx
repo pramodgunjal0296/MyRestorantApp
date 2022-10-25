e443212275df096fc13ecb31b2ffbeb2347ebcaf
Your Name <you@example.com>
2022-10-25 09:00:59 +0530

    state using reducer

--- myapp/src/components/Header.jsx
+++ myapp/src/components/Header.jsx
@@ -6,14 +6,27 @@ import {app} from '../firebase.config'
 import Avatar from '../img/avatar.png'
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 const Header = () => {
 
   const firebaseAuth= getAuth(app);
   const provider= new GoogleAuthProvider();
+
+  const [{user},dispatch]= useStateValue();
+
   const login = async() =>{
-    const response= await signInWithPopup(firebaseAuth,provider)
-    console.log(response);
+
+    if(!user){
+      const {user :{refreshToken,providerData}}= await signInWithPopup(firebaseAuth,provider)
+    dispatch({
+      type:actionType.SET_USER,
+      user:providerData[0],
+    })
+    localStorage.setItem('user',JSON.stringify(providerData[0]))
+    }
+    
 
 
 
@@ -70,11 +83,15 @@ const Header = () => {
       <div className='relative'>
       <motion.img 
       whileTap={{scale:0.6}}
-      src={Avatar} 
-      className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer'
+      src={user ? user.photoURL : Avatar} 
+      className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
        alt="userprofile"
        onClick={login}
        />
+       <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute'>
+        <p>New Item</p>
+        <p>Logout</p>
+       </div>
        </div>
       </div>
       </div>
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StateProvider } from './context/StateProvider';
+import { initialState } from './context/initialState';
+import reducer from './context/reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
   <React.StrictMode>
-   
-    <App />
-    
-  
+   <StateProvider initialState={initialState} reducer={reducer}>
+    <App/>
+   </StateProvider>
   </React.StrictMode>
   </Router>
 );
